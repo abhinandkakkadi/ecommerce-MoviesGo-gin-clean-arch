71fe4168bf7322ca4f98befafc98bb427024ab79
Abhinand KR <nanduttanvsabhi@gmail.com>
2023-06-20 20:37:49 +0530

    added domain name

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -21,7 +21,7 @@ func main() {
 	docs.SwaggerInfo.Title = "MoviesGo - E-commerce"
 	docs.SwaggerInfo.Description = "MoviesGo - E-commerce"
 	docs.SwaggerInfo.Version = "1.0"
-	docs.SwaggerInfo.Host = "localhost:3000"
+	//docs.SwaggerInfo.Host = "localhost:3000"
 	docs.SwaggerInfo.BasePath = ""
 	docs.SwaggerInfo.Schemes = []string{"http"}
 
