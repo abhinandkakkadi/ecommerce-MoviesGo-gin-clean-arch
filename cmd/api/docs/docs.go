e03902c80b27d1b1a2e0f37249a07feb22a52caf
Abhinand KR <nanduttanvsabhi@gmail.com>
2023-06-20 21:08:55 +0530

    done

--- cmd/api/docs/docs.go
+++ cmd/api/docs/docs.go
@@ -13,7 +13,7 @@ const docTemplate = `{
         "contact": {},
         "version": "{{.Version}}"
     },
-    "host": "abhinandkakkadi.com",
+    "host": "abhinandkakkadi.com:3000",
     "basePath": "{{.BasePath}}",
     "securityDefinitions": {
         "Bearer": {
